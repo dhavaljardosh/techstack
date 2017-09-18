6b646589e53e233af19e9aa91a5b61694affb3a1
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2017-09-18 07:33:50 -0500

    List FIXED, starting Action Creator

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import LibraryList from './components/LibraryList';
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View>
+      <View style={{ flex: 1 }}>
         <Header headerText="Text Stack" />
         <LibraryList />
       </View>
